7d6b65b7497c10ee6179df8b8797aafc3e397a2c
Anthony <anthony.mussard@gmail.com>
2022-05-29 17:16:33 +0400

    add modules

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+const { application } = require("express");
 
 const app = express();
 
@@ -9,6 +12,17 @@ const port = process.env.port || 8080;
 
 // log requests
 app.use(morgan("tiny"));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.get("/", (req, res) => {
   res.send("Crud appli");
